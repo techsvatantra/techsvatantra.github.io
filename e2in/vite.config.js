c255366aae446611eff7709c1abe6d22093390b9
Varun Tayur <varun.tayur@gmail.com>
2025-07-09 19:00:54 +0530

    feat: push from latest code 09-Jul

--- e2in/vite.config.js
+++ e2in/vite.config.js
@@ -190,6 +190,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	base: './',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
